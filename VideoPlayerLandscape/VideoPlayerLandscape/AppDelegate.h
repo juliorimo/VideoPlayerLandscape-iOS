baa2bb4ffb3b0b3919c727ccfb71d4a04d9302ba
Julio Rivas <julio@lualabs.com>
2015-02-24 23:35:47 +0100

    First commit

---
+++ VideoPlayerLandscape/VideoPlayerLandscape/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  VideoPlayerLandscape
+//
+//  Created by Julio Rivas on 24/2/15.
+//  Copyright (c) 2015 LuaLabs Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ VideoPlayerLandscape/VideoPlayerLandscape/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  VideoPlayerLandscape
+//
+//  Created by Julio Rivas on 24/2/15.
+//  Copyright (c) 2015 LuaLabs Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
